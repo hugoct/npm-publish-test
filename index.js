56fcc4c06e746d46b1276a708366384bd1d3222a
Hugo <hugo.ct93@gmail.com>
2019-07-25 11:09:46 -0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React from "react";
 import { NativeModules } from "react-native";
 
-export default const show = () => NativeModules.MinhaLibToast.exibirToast("Teste NPM !!");
+const show = () => NativeModules.MinhaLibToast.exibirToast("Teste NPM !!");
+
+export default show;
